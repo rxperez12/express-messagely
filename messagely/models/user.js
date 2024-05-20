c2d2fce76cda80956bd1b4cd15518ffd6419952e
Ricardo Perez <rxperez12@gmail.com>
2024-05-20 12:02:56 -0700

    added register and authenticate to user model

--- messagely/models/user.js
+++ messagely/models/user.js
@@ -1,4 +1,9 @@
 /** User of the site. */
+import { UnauthorizedError } from "../expressError.js";
+import db from '../db.js';
+import bcrypt from "bcrypt";
+import { BCRYPT_WORK_FACTOR } from "../config.js";
+
 
 class User {
 
@@ -7,11 +12,32 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const passwordHash = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users
+        (username, password, first_name, last_name, phone)
+        VALUES($1, $2, $3, $4, $5)
+        RETURNING username, password, first_name, last_name, phone`
+      [username, passwordHash, first_name, last_name, phone]
+    );
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+        FROM users
+        WHERE username = $1`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    return (user && (await bcrypt.compare(
+      password, user.password) === true));
   }
 
   /** Update last_login_at for user */
@@ -57,6 +83,11 @@ class User {
    */
 
   static async messagesTo(username) {
+  }
+
+  /** TODO: do we build a get method? */
+  async get() {
+
   }
 }
 
