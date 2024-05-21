843b3f124651439114a5440060530940472b5a2a
Jacob <jacobrashid404@gmail.com>
2024-05-20 18:05:56 -0700

    added get message, post message, read message

--- messagely/routes/messages.js
+++ messagely/routes/messages.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { ensureLoggedIn, ensureCorrectUser } from "../middleware/auth.js";
+import { UnauthorizedError } from "../expressError.js";
+import Message from "../models/message.js";
 
 const router = new express.Router();
 
@@ -15,6 +18,19 @@ const router = new express.Router();
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function (req, res) {
+  const currentUser = res.locals.user.username;
+  const msgID = req.params.id;
+  const message = await Message.get(msgID);
+
+  if (message.to_user.username !== currentUser
+    && message.from_user.username !== currentUser) {
+    throw new UnauthorizedError("You are not authorized to view this message");
+  }
+
+  return res.json({ message });
+});
+
 
 /** POST / - post message.
  *
@@ -22,6 +38,15 @@ const router = new express.Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+
+router.post("/", ensureLoggedIn, async function (req, res) {
+  const to_username = req.body.to_username;
+  const from_username = res.locals.user.username;
+  const body = req.body.body;
+
+  const message = await Message.create({ from_username, to_username, body });
+  return res.json({ message });
+});
 
 
 /** POST/:id/read - mark message as read:
@@ -31,6 +56,20 @@ const router = new express.Router();
  * Makes sure that the only the intended recipient can mark as read.
  *
  **/
+
+router.post("/:id/read", ensureLoggedIn, async function (req, res) {
+  const currentUser = res.locals.user.username;
+  const msgID = req.params.id;
+  const message = await Message.get(msgID);
+  
+  if (currentUser !== message.to_user.username) {
+    throw new UnauthorizedError("Not your message!");
+  }
+
+  const markMessageRead = await Message.markRead(msgID);
+  
+  return res.json({ message: markMessageRead });
+});
 
 
 export default router;
